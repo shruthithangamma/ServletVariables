c4ed8abcceb04265a54849cac13e2a0f335edec1
Classroom <Classroom@192.168.1.35>
2015-06-30 10:45:37 -0400

    First Commit

---
+++ SessionVariables/WebContent/index.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<table>
+<tr>
+	<th>First Name</th>
+	<th>Last Name</th>
+	<th>Email</th>
+</tr>
+	<c:forEach var="cust" items="${customerList}">
+	<tr>
+		<td><c:out value="${cust.custFirstName }" /></td>
+		<td><c:out value="${cust.custLastName }" /></td>
+		<td><c:out value="${cust.custEmail }" /></td>
+	</tr>
+	</c:forEach>
+
+</table>
+</body>
+</html>
---
+++ SessionVariables/src/Index.java
@@ -0,0 +1,61 @@
+
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Customer;
+import customTools.DBUtil;
+
+/**
+ * Servlet implementation class Index
+ */
+@WebServlet("/index")
+public class Index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Index() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = customTools.DBUtil.getEmFactory().createEntityManager();
+		String qString = "SELECT c from Customer c";
+       TypedQuery<Customer> q = em.createQuery(qString, Customer.class);
+
+       List<Customer> custs;
+       try {
+           custs = q.getResultList();
+           if (custs == null || custs.isEmpty())
+        	   custs = null;
+       } finally {
+           em.close();
+       }
+
+       request.setAttribute("customerList", custs);
+       getServletContext().getRequestDispatcher("index.jsp").forward(request, response);
+       
+	}
+
+}
